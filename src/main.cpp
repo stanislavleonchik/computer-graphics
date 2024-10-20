e113ca1829810ee76a04126c0c800d06cd3babf0
Stanislav Leonchik <leonstas1@mail.ru>
2024-10-20 13:51:58 +0300

    rename application

--- src/main.cpp
+++ src/main.cpp
@@ -11,7 +11,7 @@ int main(int argc, char** argv) {
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    GLFWwindow* window = glfwCreateWindow(display_w, display_h, "LemotkPaint", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(display_w, display_h, "Lemotech", NULL, NULL);
     if (!window) {
         std::cerr << "Failed to create GLFW window\n";
         glfwTerminate();
